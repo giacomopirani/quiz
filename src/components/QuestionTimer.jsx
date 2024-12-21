5fc8690f2d0b659cdaf5c0dc33a0d8da492d4525
Giacomo Pirani <giacomopirani1996@gmail.com>
2024-12-21 17:03:18 +0100

    modify with callback hooks

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -5,11 +5,13 @@ export default function QuestionTimer({ timeout, onTimeout}) {
     const [remainingTime, setRemainingTime] = useState(timeout);
     
     useEffect(() => {
+        console.log('SETTING TIMEOUT');
      setTimeout(onTimeout ,timeout);
     }, [timeout, onTimeout])
     
     
     useEffect(() => {
+        console.log('SETTING INTERVAL');
      setInterval(() => {
      setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
     }, 100);
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import QUESTIONS from '../../questions';
 import quizCompleteImg from '../assets/quiz-complete.png';
 import QuestionTimer from './QuestionTimer';
@@ -10,11 +10,13 @@ export default function Quiz() {
     const activeQuestionIndex = userAnswers.length;
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-    function handleSelectAnswer(selectedAnswer) {
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
        setUserAnswers((prevUserAnswers) => {
           return [...prevUserAnswers, selectedAnswer];
        });
-    }
+    }, [])
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
     if(quizIsComplete) {
         return (
@@ -31,7 +33,10 @@ export default function Quiz() {
     return (
         <div id="quiz">
            <div id="question">
-            <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)}/>
+            <QuestionTimer 
+              timeout={10000} 
+              onTimeout={handleSkipAnswer}
+            />
               <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
               <ul id="answers">
                 {shuffleAnswers.map(answer => (
